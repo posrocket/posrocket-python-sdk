4ce32f332b85ceeded7bbc5524b1bce1602f1033
ahmadiga <a.bazadough@posrocket.com>
2019-01-07 15:38:12 +0200

    webhook handler

--- posrocket/webhook/webhook_receiver.py
+++ posrocket/webhook/webhook_receiver.py
@@ -1,6 +1,9 @@
 import hashlib
 import hmac
 import json
+import logging
+
+logger = logging.getLogger("django")
 
 
 class WebhookReceiver:
@@ -8,16 +11,16 @@ class WebhookReceiver:
 
     def __init__(self,
                  pos_client,
-                 tab_create=lambda payload, headers: None,
-                 tab_update=lambda payload, headers: None,
-                 item_create=lambda payload, headers: None,
-                 item_update=lambda payload, headers: None,
-                 item_delete=lambda payload, headers: None,
-                 sale_create=lambda payload, headers: None,
-                 refund_create=lambda payload, headers: None,
-                 customer_create=lambda payload, headers: None,
-                 customer_update=lambda payload, headers: None,
-                 customer_delete=lambda payload, headers: None,
+                 tab_create=None,
+                 tab_update=None,
+                 item_create=None,
+                 item_update=None,
+                 item_delete=None,
+                 sale_create=None,
+                 refund_create=None,
+                 customer_create=None,
+                 customer_update=None,
+                 customer_delete=None,
                  ):
         self.pos_client = pos_client
         self.events["tab.create"] = tab_create
@@ -28,14 +31,16 @@ class WebhookReceiver:
         self.events["sale.create"] = sale_create
         self.events["refund.create"] = refund_create
         self.events["customer.create"] = customer_create
-        self.events["customer. update"] = customer_update
-        self.events["customer. delete"] = customer_delete
+        self.events["customer.update"] = customer_update
+        self.events["customer.delete"] = customer_delete
 
     def verify_signature(self, payload, sig):
-        hashing = hmac.new(bytearray(self.pos_client.client_secret, "utf-8"),
-                           bytearray(json.dumps(payload), "utf-8"), hashlib.sha1)
-        return hashing == sig
+        hashing = hmac.new(bytes(self.pos_client.client_secret, "utf-8"),
+                           bytes(json.dumps(payload), "utf-8"), hashlib.sha1)
+        logger.info(hashing.hexdigest())
+        logger.info(sig)
+        return hashing.hexdigest() == sig
 
     def handle(self, payload, headers):
         assert self.verify_signature(payload, headers['HTTP_X_HOOK_SIGNATURE']), "invalid signature"
-        return self.events[headers['X-Hook-Event']](payload, headers)
+        return self.events[payload['event']](payload, headers) if self.events[payload['event']] else None
