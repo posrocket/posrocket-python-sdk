bebc2a69befbb8dac8414a8a6668085319fc2d1d
ahmadiga <a.bazadough@posrocket.com>
2019-04-01 15:42:34 +0300

    include all missing attributes

---
+++ posrocket/models/timezone.py
@@ -0,0 +1,41 @@
+"""Area Python model
+
+"""
+__author__ = "Ahmad Bazadough, Hamzah Darwish"
+__copyright__ = "Copyright 2019, POSRocket"
+__credits__ = ["Ahmad Bazadough", "Hamzah Darwish"]
+__license__ = "GPL"
+__version__ = "0.1.0"
+__maintainer__ = "Ahmad Bazadough, Hamzah Darwish"
+__email__ = "a.bazadough@posrocket.com"
+__status__ = "Beta"
+
+
+class TimezoneModel:
+    """ mapper class for Area object from Json Dict
+
+    """
+    id: str
+    name: str
+    offset: int
+
+    def __init__(self,
+                 id=None,
+                 name=None,
+                 offset=None,
+                 **kwargs
+                 ):
+        """ map a dict to Area object
+
+        :param kwargs: Area json dict
+        """
+        self.id = id
+        self.name = name
+        self.offset = offset
+
+    def __str__(self) -> str:
+        """ String representation for the Area model
+
+        :return: Area name
+        """
+        return f'{self.name}'
